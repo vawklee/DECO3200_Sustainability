cb06091bbad8b196856deac4704c65e91b3a04bd
Veronica <vlee3645@uni.sydney.edu.au>
2024-10-24 01:02:15 +1100

    map functionality WIP

--- public/leafletMap.js
+++ public/leafletMap.js
@@ -6,32 +6,58 @@
 // LEAFLET MAP API SETUP; LEAFLET JS NEEDS TO STAY AT THE TOP OF THE FILE OTHERWISE IT FAILS TO REFERENCE THE SCRIPTING
 
 // Setting up the Leaflet Map with a set starting position that is showing
-var map = L.map('map').setView([-33.89209, 151.1981702], 15);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 35, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
+
+var userLocation = [-33.880466, 151.206894];
+var attributionText = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+var map = L.map('map').setView(userLocation, 15);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: attributionText}).addTo(map);
+
+// creating a circle marker to indicate the user location
+var userCircle = L.circle(userLocation, {
+    color: '#40a9e6',
+    fillColor: '#40a9e6',
+    fillOpacity: 0.35,
+    radius: 75
+}).addTo(map);
 
 // creates location marker 
-var marker = L.marker([-33.891579, 151.200532]).addTo(map);
+// var marker = L.marker([-33.891579, 151.200532]).addTo(map);
 // creates popup for that marker
-marker.bindPopup("<b>Wilkinson Building</b><br>Darlinghurst, NSW");
+// marker.bindPopup("<b>Wilkinson Building</b><br>Darlinghurst, NSW");
+
+// Temmuni Fabric Location Marker
+var markerTemmuni = L.marker([-33.8805, 151.21096]).addTo(map);
+markerTemmuni.bindPopup("<b>Temmuni Fabrics</b><br>Surry Hills, 2010");
+// Fabric Moose Location Marker
+var markerFabricM = L.marker([-33.88808, 151.19686]).addTo(map);
+markerFabricM.bindPopup("<b>Fabric Moose</b><br>Chippendale, 2008");
+// So Special Location Marker
+var markerSoSpecial = L.marker([-33.86984, 151.21013]).addTo(map);
+markerSoSpecial.bindPopup("<b>So Special</b><br>Sydney, 2000")
+
+// applying colour change filter through .css class
+markerTemmuni._icon.classList.add("markerHueChange");
+markerFabricM._icon.classList.add("markerHueChange");
+markerSoSpecial._icon.classList.add("markerHueChange");
 
 // Click event on map example
-var clickPopup = L.popup();
+var clickPopupExample = L.popup();
+map.on('click', onMapClick);
+
+markerTemmuni.on('mouseover', onMarkerHover);
+markerFabricM.on('mouseover', onMarkerHover);
+markerSoSpecial.on('mouseover', onMarkerHover);
+
 function onMapClick(e) {
     // alert(`You clicked the map at ${e.latlng}`);
-    // clickPopup.setLatLng(e.latlng).setContent(`You clicked the map at ${e.latlng.toString()}`).openOn(map);
-    clickPopup
+    // clickPopupExample.setLatLng(e.latlng).setContent(`You clicked the map at ${e.latlng.toString()}`).openOn(map);
+    clickPopupExample
     .setLatLng(e.latlng)
     .setContent("You clicked on the map at " + e.latlng.toString())
     .openOn(map);
 }
-map.on('click', onMapClick);
 
-function minimiseNavBar() {
-    var x = document.getElementById("navBarContainer");
-    if (x.className === "navBarContainer") {
-        x.className += " responsive";
-    } else {
-        x.className = "navBarContainer";
-    }
-    console.log("minimise clicked");
+// Shows the preset pop-up when cursor hover's over the marker
+function onMarkerHover(e) {
+    e.target.openPopup();
 }
--- public/script.js
+++ public/script.js
@@ -51,3 +51,14 @@ function onProfileSelectorClick(e) {
         resourcesContainer.style.display = "block";
     }
 }
+
+// disfunctional responsive code ---- WIP
+function minimiseNavBar() {
+    var x = document.getElementById("navBarContainer");
+    if (x.className === "navBarContainer") {
+        x.className += " responsive";
+    } else {
+        x.className = "navBarContainer";
+    }
+    console.log("minimise clicked");
+}
